bf6f1a6735fd3f10e6a9edca6da90938b040464e
MauryaAayush <maayush235@gmail.com>
2024-02-24 23:02:33 +0530

    Radhey Radhey

--- lib/Apps/2.dart
+++ lib/Apps/2.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
-void main()
-{
-    runApp(const Newone());
+void main() {
+  runApp(const Newone());
 }
 
 class Newone extends StatefulWidget {
@@ -16,7 +15,6 @@ class _NewoneState extends State<Newone> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       backgroundColor: Colors.transparent,
       drawer: Container(
         height: double.infinity,
@@ -27,13 +25,17 @@ class _NewoneState extends State<Newone> {
             const Padding(
               padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
               child: CircleAvatar(
-                backgroundImage: AssetImage('assets/images/me.jpg',),
+                backgroundImage: AssetImage(
+                  'assets/images/me.jpg',
+                ),
                 backgroundColor: Colors.blue,
                 radius: 50,
               ),
             ),
 
-            const SizedBox(height: 18,),
+            const SizedBox(
+              height: 18,
+            ),
 
             const Text(
               'Aayush Maurya',
@@ -53,27 +55,30 @@ class _NewoneState extends State<Newone> {
               ),
             ),
 
-            const SizedBox(height: 20,),
+            const SizedBox(
+              height: 20,
+            ),
 
             Container(
               alignment: Alignment.center,
               height: 45,
               width: 300,
               decoration: BoxDecoration(
-                color: Colors.transparent,
-                borderRadius: BorderRadius.circular(15),
-                border: Border.all(
-                  color: Colors.white,
-                  width: 1.3,
-                )
+                  color: Colors.transparent,
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(
+                    color: Colors.white,
+                    width: 1.3,
+                  )),
+              child: Text(
+                'SIGN OUT',
+                style: TextStyle(color: Colors.white, fontSize: 18),
               ),
-              child: Text('SIGN OUT',style: TextStyle(
-                color: Colors.white,
-                fontSize: 18
-              ),),
             ),
 
-            const SizedBox(height: 10,),
+            const SizedBox(
+              height: 10,
+            ),
             // OutlinedButton(
             //   child: Text("Sign ",),
             //     onPressed: () {},
@@ -94,68 +99,119 @@ class _NewoneState extends State<Newone> {
             ),
 
             const ListTile(
-              leading: Icon(Icons.edit,color: Colors.white,size: 30,),
-                    title: Text('Add Leads',style: TextStyle(
-                      color: Colors.white
-                    ),),
+              leading: Icon(
+                Icons.edit,
+                color: Colors.white,
+                size: 30,
+              ),
+              title: Text(
+                'Add Leads',
+                style: TextStyle(color: Colors.white),
+              ),
             ),
             const ListTile(
-              leading: Icon(Icons.star,color: Colors.white,size: 30,),
-              title: Text('Points Redemption',style: TextStyle(
-                  color: Colors.white
-              ),),
+              leading: Icon(
+                Icons.star,
+                color: Colors.white,
+                size: 30,
+              ),
+              title: Text(
+                'Points Redemption',
+                style: TextStyle(color: Colors.white),
+              ),
             ),
             const ListTile(
-              leading: Icon(Icons.plus_one,color: Colors.white,size: 30,),
-              title: Text('Points',style: TextStyle(
-                  color: Colors.white
-              ),),
+              leading: Icon(
+                Icons.plus_one,
+                color: Colors.white,
+                size: 30,
+              ),
+              title: Text(
+                'Points',
+                style: TextStyle(color: Colors.white),
+              ),
             ),
             const ListTile(
-              leading: Icon(Icons.person,color: Colors.white,size: 30,),
-              title: Text('Profile',style: TextStyle(
-                  color: Colors.white
-              ),),
+              leading: Icon(
+                Icons.person,
+                color: Colors.white,
+                size: 30,
+              ),
+              title: Text(
+                'Profile',
+                style: TextStyle(color: Colors.white),
+              ),
             ),
             const ListTile(
-              leading: Icon(Icons.analytics,color: Colors.white,size: 30,),
-              title: Text('Dashboard',style: TextStyle(
-                  color: Colors.white
-              ),),
+              leading: Icon(
+                Icons.analytics,
+                color: Colors.white,
+                size: 30,
+              ),
+              title: Text(
+                'Dashboard',
+                style: TextStyle(color: Colors.white),
+              ),
             ),
             const ListTile(
-              leading: Icon(Icons.home,color: Colors.white,size: 30,),
-              title: Text('Home',style: TextStyle(
-                  color: Colors.white
-              ),),
+              leading: Icon(
+                Icons.home,
+                color: Colors.white,
+                size: 30,
+              ),
+              title: Text(
+                'Home',
+                style: TextStyle(color: Colors.white),
+              ),
             ),
 
-          const Divider(color: Colors.white,height: 40,),
+            const Divider(
+              color: Colors.white,
+              height: 40,
+            ),
 
             const Padding(
               padding: EdgeInsets.fromLTRB(0, 0, 220, 10),
-              child: Text('Communicate',style: TextStyle(
-                color: Colors.white,
-              ),),
+              child: Text(
+                'Communicate',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
             ),
 
             const ListTile(
-              leading: Icon(Icons.lock,color: Colors.white,size: 30,),
-              title: Text('Privacy Policy',style: TextStyle(
-                  color: Colors.white
-              ),),
+              leading: Icon(
+                Icons.lock,
+                color: Colors.white,
+                size: 30,
+              ),
+              title: Text(
+                'Privacy Policy',
+                style: TextStyle(color: Colors.white),
+              ),
             ),
             const ListTile(
-              leading: Icon(Icons.phone,color: Colors.white,size: 30,),
-              title: Text('Contact Us',style: TextStyle(
-                  color: Colors.white
-              ),),
+              leading: Icon(
+                Icons.phone,
+                color: Colors.white,
+                size: 30,
+              ),
+              title: Text(
+                'Contact Us',
+                style: TextStyle(color: Colors.white),
+              ),
             ),
             const ListTile(
-              leading: Icon(Icons.memory_outlined,color: Colors.white,size: 30,),
-              title: Text('About App',style: TextStyle(
-                  color: Colors.white
-              ),),
+              leading: Icon(
+                Icons.memory_outlined,
+                color: Colors.white,
+                size: 30,
+              ),
+              title: Text(
+                'About App',
+                style: TextStyle(color: Colors.white),
+              ),
             ),
 
             // ListView(
@@ -168,25 +224,27 @@ class _NewoneState extends State<Newone> {
             //     )
             //   ],
             // )
-
           ],
         ),
       ),
       appBar: AppBar(
         backgroundColor: Colors.transparent,
-        iconTheme: const IconThemeData(
-          color: Colors.white
+        iconTheme: const IconThemeData(color: Colors.white),
+        title: const Text(
+          'Transparent',
+          style: TextStyle(
+            color: Colors.white,
+          ),
         ),
-        title: const Text('Transparent',style: TextStyle(
-          color: Colors.white,
-
-        ),),
         centerTitle: true,
       ),
       body: SizedBox(
         width: double.infinity,
         height: double.infinity,
-        child: Image.asset('assets/images/image.jpg',fit: BoxFit.cover,),
+        child: Image.asset(
+          'assets/images/image.jpg',
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }
